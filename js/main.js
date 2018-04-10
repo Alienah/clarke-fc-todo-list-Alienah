456caa6603bde6e42226d0c54eed86cd9ef22de4
Alienah <aidalbla@gmail.com>
2018-04-10 14:06:33 +0200

    Add date

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const tasksHeader = document.querySelector('.tasks__header');
+
+const paintDate = () => {
+  const months = new Array ("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
+  const daysWeek = new Array("Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado");
+  const date = new Date();
+  let buildDate =
+    `<div class="date__container">
+      <span class="date--number"> ${date.getDate()}</span>
+      <div>
+        <p class="date--day-week">${daysWeek[date.getDay()]}</p>
+        <p class="date--month-year">${months[date.getMonth()]}, ${date.getFullYear()}</p>
+      </div>
+    </div>`
+
+  tasksHeader.insertAdjacentHTML('beforeend', buildDate);
+}
+
+paintDate();
