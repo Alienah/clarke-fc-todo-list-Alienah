85fb19a3bb6fbe0afb5bfabf0cc02689f7049c33
Alienah <aidalbla@gmail.com>
2018-04-11 00:24:38 +0200

    Line-through when task checked

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@ const btnAddNew = document.querySelector('.btn--add-new');
 const inputTask = document.querySelector('.input-task');
 let inputTaskValue;
 const tasksListContainer = document.querySelector('.tasks');
-// let tasksArray = [];
+// const checkboxContainer = document.querySelectorAll('.checkbox');
+const taskItemContainer = document.querySelector('.task-item');
 let tasksArray = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
 
 localStorage.setItem('tasks', JSON.stringify(tasksArray) );
@@ -29,18 +30,37 @@ const paintDate = () => {
 paintDate();
 
 const paintList = (text) => {
-  const taskItem = `<li class="task-item"><input type="checkbox">${text}</li>`;
+  const taskItem = `<li class="task-item"><input type="checkbox" class="checkbox">${text}</li>`;
   tasksListContainer.insertAdjacentHTML('afterbegin', taskItem);
-}
+};
 
 const handleInputTask = (text) => {
   inputTaskValue = inputTask.value;
   paintList(inputTaskValue);
   tasksArray.push(inputTaskValue);
   localStorage.setItem('tasks', JSON.stringify(tasksArray) );
+
 }
 btnAddNew.addEventListener('click', handleInputTask);
 
 data.forEach(task => {
   paintList(task);
 });
+
+const handleCheckBox = (e) => {
+  const targetParent = e.target.parentElement;
+  console.log(targetParent);
+  if (e.target.checked == true) {
+    targetParent.classList.add('cross');
+  }
+  else {
+    targetParent.classList.remove('cross');
+  }
+}
+const setEvent = () => {
+  const checkboxContainer = document.querySelectorAll('.checkbox');
+  for (var i = 0; i < checkboxContainer.length; i++) {
+    checkboxContainer[i].addEventListener('click', handleCheckBox);
+  }
+}
+setEvent();
