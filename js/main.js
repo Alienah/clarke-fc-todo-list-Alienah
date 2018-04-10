6c35f55837157a90154350b5be3db3de41161ad4
Alienah <aidalbla@gmail.com>
2018-04-10 23:04:01 +0200

    LocalStorage running

--- js/main.js
+++ js/main.js
@@ -4,7 +4,12 @@ const tasksHeader = document.querySelector('.tasks__header');
 const btnAddNew = document.querySelector('.btn--add-new');
 const inputTask = document.querySelector('.input-task');
 let inputTaskValue;
-const tasksList = document.querySelector('.tasks');
+const tasksListContainer = document.querySelector('.tasks');
+// let tasksArray = [];
+let tasksArray = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
+
+localStorage.setItem('tasks', JSON.stringify(tasksArray) );
+const data = JSON.parse(localStorage.getItem('tasks'));
 
 const paintDate = () => {
   const months = new Array ("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
@@ -21,13 +26,21 @@ const paintDate = () => {
 
   tasksHeader.insertAdjacentHTML('beforeend', buildDate);
 }
-
 paintDate();
 
-const handleInputTask = () => {
-  inputTaskValue = inputTask.value;
-  const taskItem = `<li class="task-item">${inputTaskValue}</li>`;
-  tasksList.insertAdjacentHTML('beforeend', taskItem);
+const paintList = (text) => {
+  const taskItem = `<li class="task-item"><input type="checkbox">${text}</li>`;
+  tasksListContainer.insertAdjacentHTML('beforeend', taskItem);
 }
 
+const handleInputTask = (text) => {
+  inputTaskValue = inputTask.value;
+  paintList(inputTaskValue);
+  tasksArray.push(inputTaskValue);
+  localStorage.setItem('tasks', JSON.stringify(tasksArray) );
+}
 btnAddNew.addEventListener('click', handleInputTask);
+
+data.forEach(task => {
+  paintList(task);
+});
